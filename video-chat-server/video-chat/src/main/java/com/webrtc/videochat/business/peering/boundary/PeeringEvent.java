6c1d5964d931f829ccc0a584b916cd2849ab237f
Jahangir Zinedine <jzinedine@gmail.com>
2020-01-28 21:00:40 +0330

    finished the POC

--- video-chat-server/video-chat/src/main/java/com/webrtc/videochat/business/peering/boundary/PeeringEvent.java
+++
@@ -1,9 +0,0 @@
-package com.webrtc.videochat.business.peering.boundary;
-
-public interface PeeringEvent {
-    void onNewClient();
-
-    void onOffer(String data);
-
-    void onAnswer(String data);
-}
